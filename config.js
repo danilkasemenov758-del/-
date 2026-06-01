46e157a9b6cb9d190020612f949a9a7140ef93db
danilkasemenov758-del <danilkasemenov758@gmail.com>
2026-06-01 19:51:25 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// Пока API не развернут, оставьте пустую строку.
+// Когда backend будет опубликован, укажите его HTTPS URL.
+// API Supabase Edge Function
+window.TOCHKA_API_URL = "https://wolgzngoceroqxbyareg.functions.supabase.co/tochka-api";
